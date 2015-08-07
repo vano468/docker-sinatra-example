d5a81e712c52b501b2ea9e7ab3c146fe48391bd7
Ivan Kornilov <vano468@gmail.com>
2015-08-07 20:17:05 +0300

    Add user resources

--- app.rb
+++ app.rb
@@ -3,22 +3,22 @@ require 'data_mapper'
 
 DataMapper.setup(:default, 'postgres://localhost/sinatra_test')
 
-class Comment
+class User
   include DataMapper::Resource
 
   property :id,   Serial
-  property :body, Text
+  property :name, Text
 end
 
 set :bind, '0.0.0.0'
 set :port, '3000'
 
-post '/comments' do
-  Comment.create(body: params['body'])
+post '/users' do
+  User.create(name: params['name'])
 end
 
-get '/comments' do
-  Comment.all.map(&:body).join(',')
+get '/users' do
+  User.all.map(&:name).join(',')
 end
 
 DataMapper.finalize.auto_upgrade!
